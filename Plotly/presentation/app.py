9eeb78ea650b2867eb1c85e4b44bd0573e3d5a38
A H M SAIFUL ISLAM <iahmsaiful@gmail.com>
2023-12-19 23:52:29 +0100

    Plotly presentation Update

--- Plotly/presentation/app.py
+++ Plotly/presentation/app.py
@@ -1,7 +1,5 @@
-# From plotly --> Dash documentation
-
 # Import packages
-from dash import Dash, html, dash_table, dcc, callback, Output, Input
+from dash import Dash, html, dash_table, dcc
 import pandas as pd
 import plotly.express as px
 
@@ -13,21 +11,11 @@ app = Dash(__name__)
 
 # App layout
 app.layout = html.Div([
-    html.Div(children='My First App with Data, Graph, and Controls'),
-    html.Hr(),
-    dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='my-final-radio-item-example'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=6),
-    dcc.Graph(figure={}, id='my-final-graph-example')
+    html.Div(children='My First App with Data and a Graph'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10),
+    html.Div(children='Graphical Representation'),
+    dcc.Graph(figure=px.histogram(df, x='continent', y='lifeExp', histfunc='avg'))
 ])
-
-# Add controls to build the interaction
-@callback(
-    Output(component_id='my-final-graph-example', component_property='figure'),
-    Input(component_id='my-final-radio-item-example', component_property='value')
-)
-def update_graph(col_chosen):
-    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
-    return fig
 
 # Run the app
 if __name__ == '__main__':
